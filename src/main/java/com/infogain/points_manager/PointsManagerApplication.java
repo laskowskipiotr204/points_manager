1e43e8468acee2f8134ccb8a9c83770149338722
laskowskipiotr204 <152529080+laskowskipiotr204@users.noreply.github.com>
2023-11-30 19:40:50 +0100

    Initial commit

---
+++ src/main/java/com/infogain/points_manager/PointsManagerApplication.java
@@ -0,0 +1,13 @@
+package com.infogain.points_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PointsManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PointsManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/infogain/points_manager/PointsManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infogain.points_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PointsManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
